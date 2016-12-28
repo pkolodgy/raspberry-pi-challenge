8b68b9b9475234fb8ef7cf8a6fe30590630afd98
Patrick Kolodgy <email": null>
2016-12-28 12:16:47 -0500

    Add template

--- raspberry.py
+++ raspberry.py
@@ -1,7 +1,11 @@
 import json, requests
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/<fruit>/pie')
 def get_recipe(fruit):
