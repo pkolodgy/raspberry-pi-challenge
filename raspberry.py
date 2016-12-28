336cfa2b81a9700b9d52f0519e1e8cdf1072c81b
Patrick Kolodgy <email": null>
2016-12-28 11:35:26 -0500

    Server returning recipe json

---
+++ raspberry.py
@@ -0,0 +1,13 @@
+import json, requests
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/<fruit>/pie')
+def get_recipe(fruit):
+    data = {'q': fruit+' pie'}
+    r = requests.get("http://www.recipepuppy.com/api", params=data)
+    return app.response_class(r.content, content_type='application/json')
+
+if __name__ == "__main__":
+    app.run(threaded=True)
